d9a7c806f40256fdd99f14ad2251ef06bd828ed8
Githubhuhufeng <candy40245@yahoo.com.tw>
2017-11-25 15:39:52 +0800

    Add change port

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
      return c.String(http.StatusOK, strconv.Itoa(result))
   })
 
-    e.Logger.Fatal(e.Start(":1323"))
+    e.Logger.Fatal(e.Start(":1325"))
 }
 
 func Add(num1, num2 int) int {
